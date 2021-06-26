23ad136c2c59cb9eb97e5341671e0cf224996286
Noah <nbaculi@hotmail.com>
2021-06-26 10:32:37 -0700

    Updates

--- file_renamer.py
+++ file_renamer.py
@@ -1,14 +1,33 @@
 import os
-path = r'D:\Media\TV Shows\Welcome Back, Kotter\Season 03'  # path of the directory where the files/folders to be renamed exist
 
-string_to_be_replaced = 'Welcome Back, Kotter S03e'  # this string will be replaced by the string in the next line
-replacement_string = 'Welcome Back, Kotter - S03E'
-# replacement_string = ''
+# for ii in [2, 4]:
 
-for file in os.listdir(path):
-    if string_to_be_replaced in file:
-        try:
-            os.rename(os.path.join(path, file), os.path.join(path, file.replace(string_to_be_replaced, replacement_string)))
-            print(f'"{file}" renamed.')
-        except PermissionError:
-            print(f'*** {file} access denied and skipped.')
+
+# print(ii)
+
+path = r"D:\Media\_New\The Office\Season 10"  # + str(ii)  # path of the directory where the files/folders to be renamed exist
+
+for string_to_be_replaced, replacement_string in [['.', ' '], [' mkv', '.mkv'], ['The Office S', 'The Office - S']]:
+# for string_to_be_replaced, replacement_string in [['.WEB-DL.x264-[MULVAcoded]', '']]:
+    # string_to_be_replaced = f'.'  # this string will be replaced by the string in the next line
+    # replacement_string = f' '
+    # replacement_string = ''
+
+    for filename in os.listdir(path):
+        ## replacing substring
+        if string_to_be_replaced in filename:
+            try:
+                new_filename = filename.replace(string_to_be_replaced, replacement_string)
+                os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
+                print(f'"{filename}" renamed.')
+            except PermissionError:
+                print(f'*** {filename} access denied and skipped.')
+
+        ## renaming all files, not replacing substring
+        # try:
+        #     new_filename = f"{filename[:26] + ' - ' + filename[26:].replace(' - ', ' ')}"
+        #     os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
+        #     print(f'"{filename}" renamed.')
+        # except PermissionError:
+        #     print(f'*** {filename} access denied and skipped.')
+
---
+++ test.py
@@ -0,0 +1,3 @@
+filename = 'League of Legends - S01E01201127 - Lux Baron Steal'
+new_filename = f"{filename[:26] + ' - ' + filename[26:].replace(' - ', ' ')}"
+print(new_filename)
