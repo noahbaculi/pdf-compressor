5f784d4a7dcee3536adc5f129577048772cd5e56
Noah <nbaculi@hotmail.com>
2023-07-25 12:01:30 -0700

    file renamer usage

--- file_renamer.py
+++ file_renamer.py
@@ -1,46 +1,43 @@
 import os
 import re
 
-path = r"D:\Media\_New\Arcane (2021)\Season 01"  # + str(ii)  # path of the directory where the files/folders to be renamed exist
+path = r"A:\Pentatonix"  # + str(ii)  # path of the directory where the files/folders to be renamed exist
 
-for string_to_be_replaced, replacement_string in [
-    [" (1080p NF WEB-DL x265 t3nzin)", ""],
-    # ['S2 - ', '- S02E'],
-    # [' S2 - ', ' - S02E']
-]:
-    # for season in range(1, 10):
-    #     path = r'{}'.format(f"D:\Media\_New\Dragon Ball Z\Season {season:02d}")
-    #     string_to_be_replaced = 'Dragon Ball Z - '
-    #     replacement_string = f'Dragon Ball Z - S{season:02d}E'
 
-    filenames = os.listdir(path)
-    filenames.sort()
+filenames = os.listdir(path)
+filenames.sort()
+print(path, filenames)
+for episode, filename in enumerate(filenames):
+    ## replacing substring
+    try:
+        new_filename = filename
+        for string_to_be_replaced, replacement_string in [
+            [" - Pentatonix", ""],
+            # ["S01OAD", "S00E"],
+            # [".", " "],
+            # [" mkv", ".mkv"],
+        ]:
+            if string_to_be_replaced in new_filename:
+                new_filename = new_filename.replace(string_to_be_replaced, replacement_string)
+        print(f'"{filename}" -> "{new_filename}".')
 
-    print(path, filenames)
+        os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
+    except PermissionError:
+        print(f"*** {filename} access denied and skipped.")
 
-    for episode, filename in enumerate(filenames):
-
-        ## replacing substring
-        if string_to_be_replaced in filename:
-            try:
-                new_filename = filename.replace(string_to_be_replaced, replacement_string)
-                print(f'"{filename}" renamed to "{new_filename}".')
-
-                os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
-            except PermissionError:
-                print(f'*** {filename} access denied and skipped.')
-
-        ## renaming all files, not replacing substring
-        # try:
-        #     # index_of_dash = filename.index('-')
-        #     new_filename = filename[0:-15] + '.mkv'
-        #     # new_filename = filename.replace(f'Naruto {episode}', f'Naruto - S{season:02d}E{episode:03d}')
-        #     # new_filename = f'Fairy Tail - {episode+176:03d}'
-        #     print(new_filename)
-        #
-        #     # os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
-        # except PermissionError:
-        #     print(f'*** {filename} access denied and skipped.')
+    ## renaming all files, not replacing substring
+    # try:
+    #     # index_of_dash = filename.index('-')
+    #     if True:
+    #         # ep = int(filename[17:20])
+    #         # new_ep = ep-277
+    #         ext = filename[-4:]
+    #         new_filename = f"{filename[:-15]}{ext}"
+    #         # new_filename = filename.replace(f'Naruto {episode}', f'Naruto - S{season:02d}E{episode:03d}')
+    #         print(f'"{filename}" renamed to "{new_filename}".')
+    #
+    #         # os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
+    # except PermissionError:
+    #     print(f'*** {filename} access denied and skipped.')
 
     print()
-
--- tests.py
+++ tests.py
@@ -1,16 +1,37 @@
-def fib(series_len):
-    if series_len == 1:
-        return [0]
+import os
+import glob
+import shutil
+import sys
 
-    fib_list = [0, 1]
-    for ii in range(series_len-2):
-        fib_list.append(fib_list[-1] + fib_list[-2])
+def find_matching_directories(base_path, target_pattern):
+    matching_directories = []
+    for dir_path, dir_names, _file_names in os.walk(base_path):
+        for dir_name in dir_names:
+            full_path = os.path.join(dir_path, dir_name)
+            if glob.fnmatch.fnmatch(full_path, target_pattern):
+                matching_directories.append(full_path)
+    return matching_directories
 
-    return fib_list
 
-def sum_fib(num_fib_elements):
-    return sum(fib(num_fib_elements))
+if __name__ == "__main__":
+    base_path = input("Enter the base folder path: ")
+    target_pattern = input("Enter the target glob pattern (**b. Watch List Notes\\2023**): ")
+    source_file_path = input("Enter the source file path: ")
+
+    matching_directories = find_matching_directories(base_path, target_pattern)
+
+    print("Matching directories:")
+    for directory in matching_directories:
+        print(f"{source_file_path} copy -> {directory}")
 
 
-print(fib(4))
-print(sum_fib(4))
+    confirmation = input("Confirm (y/n): ")
+    if confirmation.lower().replace(" ", "") != "y":
+        print("Aborted.")
+        sys.exit()
+
+
+    for directory in matching_directories:
+        destination_file_path = os.path.join(directory, os.path.basename(source_file_path))
+        shutil.copy2(source_file_path, destination_file_path)
+        print(f"Copied {source_file_path} to {destination_file_path}.")
